b59aad2c837055e83eab13d449a246c4ad4a8a6b
Anna Verkhovskaya <annaverpcs@gmail.com>
2023-11-30 18:16:15 -0800

    Anna:Final commit

--- src/test/java/definitions/XpathLibrary.java
+++ src/test/java/definitions/XpathLibrary.java
@@ -8,4 +8,7 @@ public class XpathLibrary {
     public static String sEmailTextField = "//input[@id='email']" ;
     public static String sPasswordTextField = "//input[@id='password']";
     public static String sSignInButton = "//button[contains(text(),'Sign in')]";
+    public static String sMedicenterURLv1 = "https://medicenter-qa1.vercel.app/";
+    public static String sMedicenterURLv2 ="https://medicenter-qa2.vercel.app/";
+
 }
